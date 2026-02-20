63a6c9667aeac27a8e95e28c1e817d7409ed223e
weilok2021 <weilok629@gmail.com>
2026-02-20 18:03:59 +0800

    wordle is done

--- A01/wordle.c
+++ A01/wordle.c
@@ -4,9 +4,26 @@
 #include "words.h" // header that defined chooseWord()
 
 int main() {
-  srand(time(0));
-  const char* word = chooseWord();
-  printf("cheat: %s\n", word);
+    srand(time(0));
+    const char* word = chooseWord();
+    printf("cheat: %s\n", word);
 
-  return 0;
+    char *guess = malloc(50 * sizeof(char));
+    printf("Welcome to Wordle!\n");
+    printf("Please enter a 5-letter word: ");
+    scanf("%s", guess);
+    int in_word;
+    for (int i = 0; i < strlen(guess); i++) {
+        // char c;
+        in_word = 0;
+        for (int j = 0; j < strlen(word); j++) {
+            if (guess[i] == word[j]) { 
+                in_word = 1;
+                if (j != i) { printf("%c is in the word (but not in the correct spot)!\n", guess[i]); }
+            }
+        }
+        if (in_word == 0) { printf("%c is NOT in the word.\n", guess[i]); }
+    }
+
+    return 0;
 }
