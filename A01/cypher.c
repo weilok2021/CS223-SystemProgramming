91c33477e1c0d55394ce5c4ecaf9b3cb6f676b7c
weilok2021 <weilok629@gmail.com>
2026-02-20 17:21:21 +0800

    cypher exercise is done

--- A01/cypher.c
+++ A01/cypher.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void cipher(char* word, int key);
+
 int main() {
-  return 0;
+    char* word;
+    int key;
+    printf("Enter a word: ");
+    scanf("%s", word);
+    printf("Enter a shift: ");
+    scanf("%d", &key);
+    cipher(word, key);
+    printf("Your cipher is %s\n", word);
+}
+
+void cipher(char* word, int key) {   
+    for (int i = 0; i < strlen(word); i++) {
+        word[i] = (word[i] - 97 + key);
+        if (word[i] < 0) { word[i] += 26; }
+        word[i] %= 26;
+        word[i] += 97;
+    }
 }
